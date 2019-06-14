394600aa743549792d76e3219641f434e364d8ab
Joel Chen <joel123@gmail.com>
2019-06-14 10:08:08 -0700

    pack and release as zip

---
+++ pack.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const Fs = require("fs")
+const Zipit = require('zipit');
+const pkg = require("./package.json")
+
+Zipit({
+    input: [
+        './package.json',
+        './bin',
+        './dist'
+    ],
+    cwd: process.cwd()
+}, (err, buffer) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+
+    // Handle buffer, which is an instance of Buffer
+    Fs.writeFileSync(`release-${pkg.version}.zip`, buffer)
+});
