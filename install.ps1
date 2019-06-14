7733d8a59bcfd17df0ba94a3d66df7e42cc06186
Joel Chen <joel123@gmail.com>
2019-06-13 22:18:08 -0700

    powershell install script

---
+++ install.ps1
@@ -0,0 +1,132 @@
+function Find-Folders {
+    [Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") | Out-Null
+    [System.Windows.Forms.Application]::EnableVisualStyles()
+    $browse = New-Object System.Windows.Forms.FolderBrowserDialog
+    $browse.SelectedPath = "$Env:HOMEDRIVE$Env:HOMEPATH"
+    $browse.ShowNewFolderButton = $false
+    $browse.Description = "Select or create a directory for NVMW HOME"
+
+    $loop = $true
+    while($loop)
+    {
+        if ($browse.ShowDialog() -eq "OK")
+        {
+            $loop = $false
+
+		    #Insert your script here
+            return $browse.SelectedPath
+        } else
+        {
+            $res = [System.Windows.Forms.MessageBox]::Show("You clicked Cancel. Would you like to try again or exit?", "Select a location", [System.Windows.Forms.MessageBoxButtons]::RetryCancel)
+            if($res -eq "Cancel")
+            {
+                #Ends script
+                exit
+            }
+        }
+    }
+    $browse.SelectedPath
+    $browse.Dispose()
+}
+
+function Get-NodeJS($version) {
+    if ( -not (Test-Path $NVM_NODE_BIN) ) {
+        Try {
+            if ( -not (Test-Path $NVM_NODE )) {
+                New-Item -Path "$NVM_NODE" -ItemType "directory" | Out-Null
+            }
+
+            if ( Test-Path Env:NVM_NODEJS_ORG_MIRROR ) {
+                $nodejsMirror = $Env:NVM_NODEJS_ORG_MIRROR
+                Write-Output "Using Env:NVM_NODEJS_ORG_MIRROR $nodejsMirror"
+            } else {
+                $nodejsMirror = "https://nodejs.org/dist"
+            }
+            $zipFile="node-$version-win-x64.zip"
+            $nodejsBinUrl="$nodejsMirror/$version/$zipFile"
+            $destZipFile="$NVM_HOME\$zipFile"
+            "";"";"";"";"";"";
+            Write-Output "Retrieving $nodejsBinUrl"
+            Invoke-WebRequest $nodejsBinUrl -o $destZipFile
+
+            Add-Type -Assembly System.IO.Compression.FileSystem
+            $zip = [IO.Compression.ZipFile]::OpenRead($destZipFile)
+            $zip.Entries | Where-Object {$_.Name -like 'node.exe'} | 
+                ForEach-Object {[System.IO.Compression.ZipFileExtensions]::ExtractToFile($_, "$NVM_NODE_BIN", $true)}
+            $zip.Dispose()
+
+            # Expand-Archive "$destZipFile" $NVM_HOME\node
+            # Remove-Item "$destZipFile"
+        } Catch {
+            Write-Error -Message "Retrieving node version $version binary failed  $_.Exception.Message"
+            exit
+        }
+    }
+}
+
+function Add-Path {
+    <#
+      .SYNOPSIS
+        Adds a Directory to the Current Path
+      .DESCRIPTION
+        Add a directory to the current path.  This is useful for 
+        temporary changes to the path or, when run from your 
+        profile, for adjusting the path within your powershell 
+        prompt.
+      .EXAMPLE
+        Add-Path -Directory "C:\Program Files\Notepad++"
+      .PARAMETER Directory
+        The name of the directory to add to the current path.
+    #>
+  
+    [CmdletBinding()]
+    param (
+      [Parameter(
+        Mandatory=$True,
+        ValueFromPipeline=$True,
+        ValueFromPipelineByPropertyName=$True,
+        HelpMessage='What directory would you like to add?')]
+      [Alias('dir')]
+      [string[]]$Directory
+    )
+  
+    PROCESS {
+      $Path = $env:PATH.Split(';').Where({ $_ -ne "" })
+
+      foreach ($dir in $Directory) {
+        if ($Path -contains $dir) {
+            Write-Verbose "$dir is already present in PATH"
+        } else {
+            $Path += $dir
+        }
+      }
+  
+      $newPath = [String]::Join(';', $Path)
+      $Env:Path="$newPath"
+    }
+  }
+
+if ( Test-Path Env:NVM_HOME ) {
+    $NVM_HOME = $Env:NVM_HOME
+    Write-Output "Got NVM_HOME $NVM_HOME from Env"
+} else {
+    $NVM_HOME = Find-Folders "$Env:USERPROFILE"
+    Write-Output "Selected folder for NVM_HOME $NVM_HOME"
+}
+
+$NVM_NODE="$NVM_HOME\node"
+$NVM_NODE_BIN="$NVM_NODE\node.exe"
+
+if ( -not (Test-Path $NVM_NODE_BIN ) ) {
+    Get-NodeJS "v10.16.0"
+}
+
+Add-Path -Directory "$NVM_HOME"
+$Env:NVM_LINK="$Env:USERPROFILE\nodejs"
+$Env:NVM_HOME=$NVM_HOME
+New-ItemProperty -Path "HKCU:\Environment" -Name "Path" -Value "$Env:Path" -Force | Out-Null
+New-ItemProperty -Path "HKCU:\Environment" -Name "NVM_HOME" -Value "$NVM_HOME" -Force | Out-Null
+# use setx once so it broadcasts WM_SETTINGCHANGE message
+setx.exe NVM_LINK "$Env:USERPROFILE\nodejs" | Out-Null
+
+# $NVM_NODE\node.exe setup.js
