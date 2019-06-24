d0e21775191d14ece41dbbdbfb42a510917bbdbf
Joel Chen <xchen@walmartlabs.com>
2019-06-24 11:28:34 -0700

    handle tgz file extraction

--- bin/install_bashrc.js
+++ bin/install_bashrc.js
@@ -10,16 +10,20 @@ const profileFile = process.argv[2] || path.join(homeDir, ".bash_profile");
 
 const profile = fs.existsSync(profileFile) ? fs.readFileSync(profileFile, "utf8").split("\n") : [];
 
-const nvmHome = process.env.NVM_HOME;
+const nvmHome = process.env.NVM_HOME || `${homeDir}/nvm`;
+const nvmLink = process.env.NVM_LINK || `${nvmHome}/nodejs/bin`;
 
 const homeAlias = process.env.HOME ? "${HOME}" : "~";
 
 const varNvmHome = nvmHome.replace(homeDir, homeAlias);
+const varNvmLink = nvmLink.replace(homeDir, homeAlias);
 
 const setNvmHomeCmd = `export NVM_HOME="${varNvmHome}"`;
+const setNvmLinkCmd = `export NVM_LINK="${varNvmLink}"`;
 
 if (profile.indexOf(setNvmHomeCmd) < 0) {
   profile.push(setNvmHomeCmd);
+  profile.push(setNvmLinkCmd);
   profile.push(`source "\$\{NVM_HOME}/bin/nvm.sh"\n`);
 
   fs.writeFileSync(profileFile, profile.join("\n"));
