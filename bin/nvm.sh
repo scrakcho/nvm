5e6dee41ca5240feaa616462493e2485c894f8f7
Joel Chen <xchen@walmartlabs.com>
2019-06-26 00:46:29 -0700

    bash quote vars

--- bin/nvm.sh
+++ bin/nvm.sh
@@ -1,29 +1,29 @@
 function nvm() {
-  if [ -z $NVM_HOME ]; then
+  if [ -z "$NVM_HOME" ]; then
     NVM_HOME=~/nvm
   fi
 
-  NVM_NODE=$NVM_HOME/node
-  if [ ! -x $NVM_NODE ]; then
+  NVM_NODE="$NVM_HOME/node"
+  if [ ! -x "$NVM_NODE" ]; then
     NVM_NODE=$(which node)
   fi
 
-  if [ ! -x $NVM_NODE ]; then
+  if [ ! -x "$NVM_NODE" ]; then
     echo "Can't locate default node executable";
     return 1
   fi
 
-  $NVM_NODE $NVM_HOME/dist/nvm.js $*
+  $NVM_NODE "$NVM_HOME/dist/nvm.js" $*
 
-  if [ -z $TMPDIR ]; then
+  if [ -z "$TMPDIR" ]; then
     TMPDIR=/tmp
   fi
 
-  if [ -f $TMPDIR/nvm_env.sh ]; then
-    rm -rf $TMPDIR/nvm_envx.sh
-    mv $TMPDIR/nvm_env.sh $TMPDIR/nvm_envx.sh
-    source $TMPDIR/nvm_envx.sh
-    rm -rf $TMPDIR/nvm_envx.sh
+  if [ -f "$TMPDIR/nvm_env.sh" ]; then
+    rm -rf "$TMPDIR/nvm_envx.sh"
+    mv "$TMPDIR/nvm_env.sh" "$TMPDIR/nvm_envx.sh"
+    source "$TMPDIR/nvm_envx.sh"
+    rm -rf "$TMPDIR/nvm_envx.sh"
   fi
 
   return 0
@@ -32,5 +32,5 @@ function nvm() {
 # If a version is linked, then automatically add it to PATH
 
 if [ -d "$NVM_LINK" ] && [ -x "$NVM_LINK/node" ]; then
-  export PATH=$NVM_LINK:$PATH
+  export PATH="$NVM_LINK:$PATH"
 fi
