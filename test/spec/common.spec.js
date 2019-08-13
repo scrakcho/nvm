a94018481eac154a06798732831475c6df45b486
Joel Chen <xchen@walmartlabs.com>
2019-08-12 21:38:08 -0700

    retrieve remote versions from index.json

--- test/spec/common.spec.js
+++ test/spec/common.spec.js
@@ -1,1 +1,9 @@
-//
+"use strict";
+
+const common = require("../../lib/common");
+
+describe("common.getRemoteFromJson", function() {
+  it("should get versions", async () => {
+    await common.getRemoteFromJson();
+  });
+});
