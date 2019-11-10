2ca64e3b943197292135529609b5f97e951041b1
Joel Chen <joel123@gmail.com>
2019-11-10 15:01:12 -0800

    fix windows link config

--- bin/nvm.ps1
+++ bin/nvm.ps1
@@ -1,22 +1,23 @@
-$Env:NVM_POWERSHELL = 1
+$Env:NVM_POWERSHELL = "true"
+$Env:NVM_PSPROFILE = "$PROFILE.CurrentUserAllHosts"
 
 Try {
-    $NODE_EXE = "$PSScriptRoot\..\node.exe"
-    if ( -not (Test-Path $NODE_EXE)) {
-        $NODE_EXE = node
-    }
+  $NODE_EXE = "$PSScriptRoot\..\node.exe"
+  if ( -not (Test-Path $NODE_EXE)) {
+    $NODE_EXE = node
+  }
 
-    $NVM_JS = "$PSScriptRoot\..\dist\nvm.js"
+  $NVM_JS = "$PSScriptRoot\..\dist\nvm.js"
 
-    & $NODE_EXE $NVM_JS $args[0] $args[1] $args[2] $args[3]
+  & $NODE_EXE $NVM_JS $args[0] $args[1] $args[2] $args[3]
 }
 Finally {
-    Remove-Item Env:\NVM_POWERSHELL
+  Remove-Item Env:\NVM_POWERSHELL
 }
 
 $nvmEnv = "$Env:TMP\nvm_env.ps1"
 
 if ( Test-Path $nvmEnv ) {
-    & $nvmEnv
-    Remove-Item -Path $nvmEnv
+  & $nvmEnv
+  Remove-Item -Path $nvmEnv
 }
