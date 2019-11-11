2e3a4f649eb1e9a13f474e9ffee54cc810311804
Joel Chen <joel123@gmail.com>
2019-11-10 19:55:07 -0800

    delete link path if unlink

--- test/spec/common.spec.js
+++ test/spec/common.spec.js
@@ -3,6 +3,7 @@
 const common = require("../../lib/common");
 
 describe("common.getRemoteFromJson", function() {
+  this.timeout(10000);
   it("should get versions", async () => {
     await common.getRemoteFromJson();
   });
